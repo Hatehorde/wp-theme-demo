2efa1a2059fd03f62344a8044898a82bbd568cfe
Hatehorde <1963113@cgmatane.qc.ca>
2022-02-07 16:35:13 -0500

    Add theme fles

---
+++ footer.php
@@ -0,0 +1,11 @@
+</main>
+    <footer class="flex v-aligne">
+        <div class="conteneur">
+            <p><?php bloginfo( 'title' ) ?>, tous droits réservés &copy <?php echo(date('Y')) ?></p>
+        </div>
+
+    </footer>
+    
+</body>
+<?php wp_footer(  ); ?>
+</html>
